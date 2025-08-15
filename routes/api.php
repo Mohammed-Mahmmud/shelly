1722eac866a2ca66d8abf7f453026d86f4d7405a
Mohamed Khater <121749484+Mohammed-Mahmmud@users.noreply.github.com>
2025-08-15 22:22:23 +0300

    updae

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::controller(FrontController::class)->name('api.')->group(function () {
+    Route::get('producs-filter', 'productsFilter')->name('productsFilter');
+    Route::get('page/home', 'getHomePage')->name('home');
+    Route::get('settings', 'settings')->name('settings');
+    Route::post('products/search', 'search')->name('products.search');
     Route::get('products/{id?}', 'products')->name('products');
     Route::get('solutions/{page?}', 'solutions')->name('solutions');
     Route::get('projects/{id?}', 'projects')->name('projects');
     Route::get('project/{id}/', 'project')->name('project');
     Route::get('navbar/{slug?}', 'navbar');
-    Route::get('producs-filter', 'productsFilter')->name('productsFilter');
-    Route::get('page/home', 'getHomePage')->name('home');
-    Route::get('settings', 'settings')->name('settings');
-    Route::post('products/search', 'search')->name('products.search');
 });
